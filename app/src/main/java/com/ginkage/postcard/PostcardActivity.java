c91ff323e3de38c20414288b731cb96635641433
Ivan Podogov <ginkage@yandex.ru>
2017-10-23 00:05:15 +0100

    Upgrade Android Studio, target to watches.

--- app/src/main/java/com/ginkage/postcard/PostcardActivity.java
+++ app/src/main/java/com/ginkage/postcard/PostcardActivity.java
@@ -2,11 +2,9 @@ package com.ginkage.postcard;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class PostcardActivity extends Activity {
 	private PostcardSurfaceView mGLView = null;
-	private final Handler mHandler = new Handler();
 
 	/** Called when the activity is first created. */
 	@Override
@@ -14,52 +12,26 @@ public class PostcardActivity extends Activity {
     {
 		super.onCreate(savedInstanceState);
 
-        // Create a GLSurfaceView instance and set it
-		// as the ContentView for this Activity
 		mGLView = new PostcardSurfaceView(this);
 		setContentView(mGLView);
-
-		mHandler.removeCallbacks(mUpdateTimeTask);
-		mHandler.postDelayed(mUpdateTimeTask, 1000);
 	}
 
 	@Override
 	protected void onPause()
     {
 		super.onPause();
-		// The following call pauses the rendering thread.
-		// If your OpenGL application is memory intensive,
-		// you should consider de-allocating objects that
-		// consume significant memory here.
-		if (mGLView != null)
+		if (mGLView != null) {
 			mGLView.onPause();
-		mHandler.removeCallbacks(mUpdateTimeTask);
+		}
 	}
 
 	@Override
 	protected void onResume()
     {
 		super.onResume();
-		// The following call resumes a paused rendering thread.
-		// If you de-allocated graphic objects for onPause()
-		// this is a good place to re-allocate them.
-		if (mGLView != null)
-			mGLView.onResume();
-		mHandler.removeCallbacks(mUpdateTimeTask);
-		mHandler.postDelayed(mUpdateTimeTask, 1000);
+		if (mGLView != null) {
+            mGLView.onResume();
+        }
 	}
-
-	private final Runnable mUpdateTimeTask = new Runnable() {
-		public void run() {
-			if (mGLView != null) {
-				if (mGLView.mRenderer != null) {
-					float fps = mGLView.mRenderer.fps;
-					setTitle(String.format("%.1f FPS", fps));
-				}
-			}
-
-			mHandler.postDelayed(this, 1000);
-		}
-	};
 }
 
--- app/src/main/java/com/ginkage/postcard/PostcardSurfaceView.java
+++ app/src/main/java/com/ginkage/postcard/PostcardSurfaceView.java
@@ -4,16 +4,9 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 class PostcardSurfaceView extends GLSurfaceView {
-	public PostcardRenderer mRenderer = null;
-
 	public PostcardSurfaceView(Context context) {
 		super(context);
-
-		// Create an OpenGL ES 2.0 context.
 		setEGLContextClientVersion(2);
-
-		// Set the Renderer for drawing on the GLSurfaceView
-		mRenderer = new PostcardRenderer(context);
-		setRenderer(mRenderer);
+		setRenderer(new PostcardRenderer(context));
 	}
 }
